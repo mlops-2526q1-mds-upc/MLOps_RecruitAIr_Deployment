c49cf4c2cf2cdce0077e5d508255defdbe1dd9f8
abrowng <abrown.glez@gmail.com>
2025-12-06 18:37:42 +0100

    Fix app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 
 import aws_cdk
-from cdk.stack import RecruitAirEksStack, EksAlbStack
+from cdk.stack import EksAlbStack
 
 chart_path = "./recruitair"
 
@@ -9,6 +9,6 @@ if not os.path.exists(chart_path):
     raise FileNotFoundError(f"Could not find Helm chart at: {chart_path}")
 
 app = aws_cdk.App()
-# RecruitAirEksStack(app, "RecruitAirEksStack", chart_path)
+
 EksAlbStack(app, "RecruitAirEksStack")
 app.synth()
